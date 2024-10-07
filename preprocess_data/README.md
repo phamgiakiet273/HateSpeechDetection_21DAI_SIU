7df6903bab3bece7b7d418eadc018b015582b5e9
QKhanhGT6 <153789536+QKhanhGT6@users.noreply.github.com>
2024-10-07 17:06:30 +0700

    Upload preprocess data folder
    Folder includes py coding file, instruction file, and dependencies

---
+++ preprocess_data/README.md
@@ -0,0 +1,38 @@
+﻿# Preprocessing data for Hate Speech Detection in YouTube Comments
+
+## Overview
+
+This module preprocesses YouTube comments/transcripts for hate speech detection models. It includes steps to clean, filter, and standardize comments, making them ready for further analysis or model input. The preprocessing ensures that comments retain key features important for detecting offensive or hate speech, while irrelevant elements are removed or adjusted.
+
+Input: a list of comments/transcripts
+Output: a list of preprocessed comments/transcripts
+
+## Features
+
+-   **Filtering non-English comments**: Filters out comments that are not in English (label isn't `__label__eng_Latn`) using facebook/fasttext-language-identification.
+-  **Cleaning comments**: The function performs several cleaning tasks, including the removal of unnecessary punctuation using RegEx,  replacing numbers with corresponding letters to handle leetspeak (e.g., "h4t3" becomes "hate"), and ensuring that comments contain at least 2 letters.
+
+**No spell-checking**: In toxic comments, there might be a correlation between misspelled text and toxicity. Spell-checking system might auto-correct hate speech terms / misspelled texts into completely different words.
+
+## Requirements
+
+To set up and run the project, install the following Python libraries using `pip`:
+
+```bash
+pip install -r requirements.txt
+```
+
+## How to use this module
+
+```python
+from hateSD_preprocess import preprocess_comments
+
+"""
+Change path as need
+"""
+json_path = "/content/drive/MyDrive/Colab Notebooks/datasets/trivial/save_comment_example.json"
+output_csv_path = "/content/drive/MyDrive/Colab Notebooks/datasets/trivial/cleaned_comments.csv"
+
+preprocess_comments(json_path, output_csv_path)
+ ```
+
---
+++ preprocess_data/hateSD_preprocess.py
@@ -0,0 +1,54 @@
+import re
+import fasttext
+from huggingface_hub import hf_hub_download
+import json
+
+# Download and load the fastText model for language identification
+model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
+model = fasttext.load_model(model_path)
+
+def preprocess_comments(json_path, output_csv_path):
+
+    def is_english(comment):
+        """ Function to filter non-English comments """
+        prediction = model.predict(comment, k=2) # Get the top 2 languages present
+        for label in prediction[0]:
+            if label == "__label__eng_Latn":
+                return True
+        return False
+
+    # Load the JSON file with utf-8-sig encoding
+    with open(json_path, 'r', encoding='utf-8-sig') as f:
+        comments_data = json.load(f)
+    comments = comments_data
+
+    cleaned_comments = []
+    number_to_letter_map = {
+        '0': 'o',
+        '1': 'l',
+        '3': 'e',
+        '4': 'a',
+        '5': 's',
+        '7': 't',
+        '9': 'g'
+    }
+
+    for comment in comments:
+        # Remove punctuation (including emoji, except necessary ones like: f**k, sh!t, @$$)
+        comment = comment.replace('\n', ' ')
+        comment = re.sub(r'[^\w\s*?!@$&]', '', comment)
+        comment = ''.join([number_to_letter_map.get(c, c) if c.isdigit() else c for c in comment])
+
+        # Check if the comment contains at least 2 letters
+        letters = [char for char in comment if char.isalpha()]
+        if len(letters) >= 2 and is_english(comment):
+            cleaned_comments.append(comment)
+
+    import csv
+
+    # Save cleaned comments directly to a CSV file
+    with open(output_csv_path, mode='w', newline='', encoding='utf-8-sig') as file:
+        writer = csv.writer(file)
+        writer.writerow(["cleaned_comments"])  # Write the header
+        for comment in cleaned_comments:
+            writer.writerow([comment])  # Write each comment as a row
