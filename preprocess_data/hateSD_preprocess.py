0384e549f7c49b70b11ca57e534ffdbf4d12c779
QKhanhGT6 <153789536+QKhanhGT6@users.noreply.github.com>
2024-10-21 09:05:16 +0700

    Add NER to preprocess function

--- preprocess_data/hateSD_preprocess.py
+++ preprocess_data/hateSD_preprocess.py
@@ -6,15 +6,97 @@ import json
 import csv
 import pandas as pd
 
+from emoji import demojize
+from nltk.tokenize import TweetTokenizer
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
 # Download and load the fastText model for language identification
 model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
-model = fasttext.load_model(model_path)
+model2 = fasttext.load_model(model_path)
 
-def preprocess(comment_path, transcript_path, output_comment_path, output_transcript_path):
+tokenizer = TweetTokenizer()
+
+def normalizeToken(token):
+    lowercased_token = token.lower()
+    if token.startswith("@"):
+        return "@USER"
+    elif lowercased_token.startswith("http") or lowercased_token.startswith("www"):
+        return "HTTPURL"
+    elif len(token) == 1:
+        return demojize(token)
+    else:
+        if token == "’":
+            return "'"
+        elif token == "…":
+            return "..."
+        else:
+            return token
+
+def normalizeTweet(tweet):
+    tokens = tokenizer.tokenize(tweet.replace("’", "'").replace("…", "..."))
+    normTweet = " ".join([normalizeToken(token) for token in tokens])
+
+    normTweet = (
+        normTweet.replace("cannot ", "can not ")
+        .replace("n't ", " n't ")
+        .replace("n 't ", " n't ")
+        .replace("ca n't", "can't")
+        .replace("ai n't", "ain't")
+    )
+    normTweet = (
+        normTweet.replace("'m ", " 'm ")
+        .replace("'re ", " 're ")
+        .replace("'s ", " 's ")
+        .replace("'ll ", " 'll ")
+        .replace("'d ", " 'd ")
+        .replace("'ve ", " 've ")
+    )
+    normTweet = (
+        normTweet.replace(" p . m .", "  p.m.")
+        .replace(" p . m ", " p.m ")
+        .replace(" a . m .", " a.m.")
+        .replace(" a . m ", " a.m ")
+    )
+
+    return " ".join(normTweet.split())
+
+if __name__ == "__main__":
+    print(
+        normalizeTweet(
+            "SC has first two presumptive cases of coronavirus, DHEC confirms https://postandcourier.com/health/covid19/sc-has-first-two-presumptive-cases-of-coronavirus-dhec-confirms/article_bddfe4ae-5fd3-11ea-9ce4-5f495366cee6.html?utm_medium=social&utm_source=twitter&utm_campaign=user-share… via @postandcourier"
+        )
+    )
+
+tokenizer = AutoTokenizer.from_pretrained("TweebankNLP/bertweet-tb2_wnut17-ner")
+model = AutoModelForTokenClassification.from_pretrained("TweebankNLP/bertweet-tb2_wnut17-ner")
+#ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer, grouped_entities=True)
+ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
+
+def remove_entities(comment):
+    # Get named entities in the comment
+    ner_results = ner_pipeline(comment)
+
+    # Extract entity words to remove
+    entity_words = [result['word'].replace('@@', '') for result in ner_results]  # Remove '@@' from subword tokens
+
+    # Create a regex pattern to match entity words
+    pattern = r'\b(' + '|'.join(re.escape(word) for word in entity_words) + r')\b'
+
+    # Replace entity words with an empty string
+    new_comment = re.sub(pattern, '', comment)
+
+    # Clean up extra spaces
+    new_comment = re.sub(r'\s+', ' ', new_comment).strip()
+
+    return new_comment
+
+
+def preprocess(comment_path, transcript_path, output_comment_path, output_transcript_path, output_NER_comment, output_NER_transcript):
 
     def is_english(text):
         """ Function to filter non-English text """
-        prediction = model.predict(text, k=2) # Get the top 2 languages present
+        prediction = model2.predict(text, k=2) # Get the top 2 languages present
         for label in prediction[0]:
             if label == "__label__eng_Latn":
                 return True
@@ -58,7 +140,7 @@ def preprocess(comment_path, transcript_path, output_comment_path, output_transc
         if len(letters) >= 2 and is_english(comment):
             cleaned_comments.append((comment, time))
 
-    # Save cleaned comments directly to a CSV file
+    # Save cleaned comments to a CSV file
     with open(output_comment_path, mode='w', newline='', encoding='utf-8-sig') as file:
         writer = csv.writer(file)
         for comment, time in cleaned_comments:
@@ -72,8 +154,26 @@ def preprocess(comment_path, transcript_path, output_comment_path, output_transc
         if len(letters) >= 2 and is_english(sentence):
             cleaned_transcript.append(sentence)
 
-    # Save cleaned transcript directly to a CSV file
     with open(output_transcript_path, mode='w', newline='', encoding='utf-8-sig') as file:
         writer = csv.writer(file)
         for text in cleaned_transcript:
-            writer.writerow([text])  # Write each comment as a row
+            writer.writerow([text])
+
+    # ===== add NER =====
+    cleaned_comments_df = pd.DataFrame(cleaned_comments)
+    cleaned_transcript_df = pd.DataFrame(cleaned_transcript)
+
+    NER_cleaned_comments = cleaned_comments_df[0].apply(normalizeTweet)
+    NER_cleaned_transcript = cleaned_transcript_df[0].apply(normalizeTweet)
+
+    NER_cleaned_comments = cleaned_comments_df[0].apply(remove_entities)
+    NER_cleaned_transcript = cleaned_transcript_df[0].apply(remove_entities)
+
+    with open(output_NER_comment, mode='w', newline='', encoding='utf-8-sig') as file:
+        writer = csv.writer(file)
+        for comment in NER_cleaned_comments:
+            writer.writerow([comment])
+    with open(output_NER_transcript, mode='w', newline='', encoding='utf-8-sig') as file:
+        writer = csv.writer(file)
+        for sentence in NER_cleaned_transcript:
+            writer.writerow([sentence])
