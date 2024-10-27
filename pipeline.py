8ef19363a6d4db4812c2057db0d96a7ab6ec2af7
phamgiakiet273 <phamgiakiet273@gmail.com>
2024-10-27 19:43:05 +0700

    Add files via upload

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,10 @@
 import sys
 import os
 import shutil
+import pickle
+from flask import Flask, render_template, send_file, request, redirect, url_for
 
-from preprocess_data.hateSD_preprocess import preprocess
+from DataPreprocessing.hateSD_preprocess import preprocess
 from YoutubeToText.download_youtube import save_video_from_link
 from YoutubeToText.extract_comment import save_comment_from_video_link
 from YoutubeToText.extract_transcription import save_transcript_from_video_path
@@ -34,7 +36,7 @@ class HateSpeechDetection:
         print("Preprocess Completed")
         
         
-video_link = "https://www.youtube.com/watch?v=hytigOSjJxc"
+# video_link = "https://www.youtube.com/watch?v=hytigOSjJxc"
 video_save_path="E:/HateSpeechDetection_21DAI_SIU/samples/videos"
 wav_path="E:/HateSpeechDetection_21DAI_SIU/samples/videos/wav_path"
 
@@ -49,13 +51,44 @@ output_cleaned_NER_transcript_path = "E:/HateSpeechDetection_21DAI_SIU/samples/c
 hateSpeechDetection = HateSpeechDetection()
 
 
+app = Flask(__name__, template_folder='E:/HateSpeechDetection_21DAI_SIU/WebUI/templates', static_folder='E:/HateSpeechDetection_21DAI_SIU/WebUI/static')
+path_parent = os.getcwd()
 
-hateSpeechDetection.video_to_comment_and_transcript(video_link=video_link,
-                                                    video_save_path=video_save_path,
-                                                    wav_path=wav_path,
-                                                    output_raw_comment_path=output_raw_comment_path,
-                                                    output_raw_transcript_path=output_raw_transcript_path)
 
-hateSpeechDetection.preprocess_comment_and_transcript(output_raw_comment_path, output_raw_transcript_path, 
-           output_cleaned_comment_path, output_cleaned_transcript_path, 
-           output_cleaned_NER_comment_path, output_cleaned_NER_transcript_path)
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/download')
+def download_file():
+
+    path = path_parent + '/result.csv'
+    return send_file(path, as_attachment=True)
+
+@app.route('/process', methods=['POST'])
+def video_processing():
+    if request.method == 'POST':
+        video_path = request.form['youtube-url']
+        hateSpeechDetection.video_to_comment_and_transcript(video_link=video_path,
+                                                            video_save_path=video_save_path,
+                                                            wav_path=wav_path,
+                                                            output_raw_comment_path=output_raw_comment_path,
+                                                            output_raw_transcript_path=output_raw_transcript_path)
+        
+        print('Done')
+
+
+@app.route('/predict', methods=['POST'])
+def custom():
+    
+    path_csv = request.form['task']    
+    # Classify toxic text
+    if request.method == 'POST':
+        hateSpeechDetection.preprocess_comment_and_transcript(output_raw_comment_path, output_raw_transcript_path, 
+                output_cleaned_comment_path, output_cleaned_transcript_path, 
+                output_cleaned_NER_comment_path, output_cleaned_NER_transcript_path)
+        
+    return render_template('home.html', message="Processing complete. You can download the results.")
+
+if __name__ == "__main__":
+    app.run(port=8502, debug=False)
